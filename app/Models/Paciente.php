7510980fd2812a15d94ec2a7e286b935f4a85b53
Francisco Portales <franciscoportales8912748@gmail.com>
2023-10-29 22:03:03 -0600

    MODELO DE

---
+++ app/Models/Paciente.php
@@ -0,0 +1,13 @@
+<?php 
+namespace App\Models;
+
+use CodeIgniter\Model;
+class Ciudadano extends Model{
+    protected $table      = 'pacientes';
+    // Uncomment below if you want add primary key
+     protected $primaryKey = 'cod_paciente';
+    protected $allowedFields=[ 'cod_paciente','cod_estado_civil','cod_profesion','cod_pais','cod_departamento','cod_municipio','cod_genero','fecha_alta','fecha_nacimiento','lugar_nacimiento','telefono','cui','nit','nombre1','nombre2','nombre3','apellido1','apellido2','email','direccion','penicilina','polvo','moho','abeja','observaciones','padre','madre',
+    'conyuge',    'cuim',    'cuip',    'cuic','nombre_factura','nit_factura'
+  ];
+
+}
